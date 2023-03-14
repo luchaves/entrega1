cac6d928736634eabdd30b36439ad929fbd816da
Lucas Chaves <lunechaves@gmail.com>
2023-03-14 10:54:57 -0300

    Primera entrega JS

---
+++ entrega1.js
@@ -0,0 +1,52 @@
+//Función saludar
+
+let nombre=prompt('Cual es tu nombre?');
+    alert('Bienvenida/o '+nombre);
+
+//Ejercicio CONDICIONAL
+
+/* let color= prompt('Ingresa el nombre del color y te diré su clasificación (s para salir)');
+while(color != 's'){
+    switch(color){
+        case 'rojo':
+        alert('El color rojo es PRIMARIO');
+        break;
+        case 'amarillo':
+        alert('El color amarillo es PRIMARIO');
+        break;
+        case 'azul':
+        alert('El color azul es PRIMARIO');
+        break;
+        case 'naranja':
+        alert('El color naranja es SECUNDARIO');  
+        break;
+        case 'verde':
+        alert('El color verde es SECUNDARIO');  
+        break;
+        case 'violeta':
+        alert('El color violeta es SECUNDARIO');  
+        default:
+        alert('Color no identificado');
+        break;     
+    }color= prompt('Ingresa el nombre del color y te diré su clasificación (s para salir)');
+} */
+
+//Ejercicio CICLO + CONDICIONAL
+
+/* contador()
+function contador() {
+    for (let  i=1; i <= 5; i++) {
+        let edad = parseFloat(prompt('Ingresa antiguedad de años trabajados del empleado n° ' + i));
+
+        if (edad <= 2) {
+            alert('El empleado es Junior')
+        } else if (edad <= 5) {
+            alert('El empleado es Semi senior')
+        } else if (edad > 5) {
+            alert('El empleado es Senior')
+        } else {
+            alert('Ingrese formato de año correcta!!')
+            contador()
+        }
+    }
+} */
